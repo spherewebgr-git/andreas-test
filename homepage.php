1d4de655281752d8924290f50824ad2c35664c73
Spiros Karagiannis <s.karagiannis@egov.demokritos.gr>
2026-03-05 10:54:35 +0200

    homepage first elements

---
+++ homepage.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: Home Page
+ * @package Andrew Theme
+ * @since 1.0
+ * @version 2.0
+ * home page branch version
+ */
+?>
+<?php get_header()
+. get_template_part('parts/homepage/section', 'services')
+.get_footer(); ?>
