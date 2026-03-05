55d054bed4bcdc016eb2e602fc0fa50c4ea2e62b
Andrew <andrew.jevtovic@gmail.com>
2026-03-05 11:34:20 +0200

    slider js

--- parts/homepage/section-slider.php
+++ parts/homepage/section-slider.php
@@ -1,7 +1,23 @@
 <div id="slider">
-    <div class="container">
-        <div class="row">
-            slider
+    <div class="slide">
+        <div class="container">
+            <div class="row">
+                slider1
+            </div>
+        </div>
+    </div>
+    <div class="slide">
+        <div class="container">
+            <div class="row">
+                slider2
+            </div>
+        </div>
+    </div>
+    <div class="slide">
+        <div class="container">
+            <div class="row">
+                slider3
+            </div>
         </div>
     </div>
 </div>
