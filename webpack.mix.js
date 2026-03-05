acd8ac86f4a74c32da1bb8f2569659ebd214053b
Andrew <andrew.jevtovic@gmail.com>
2026-03-05 10:40:58 +0200

    add mix support

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,8 +8,8 @@ mix.options({
     withFileTypes: true
 });
 
-fs.readdirSync("assets/sass/").forEach(fileName => {
-    mix.sass(`assets/sass/${fileName}`, "assets/css", {
+fs.readdirSync("assets/scss/").forEach(fileName => {
+    mix.sass(`assets/scss/${fileName}`, "assets/css", {
         sassOptions: {
             silenceDeprecations: ['legacy-js-api']
         }
